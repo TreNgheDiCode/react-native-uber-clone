66ad22914073d12f920bb1bc2d7cf0993dca4088
TreNgheDiCode <gabayan170@gmail.com>
2024-09-24 13:38:07 +0700

    Onboarding Screens

---
+++ components/custom-button.tsx
@@ -0,0 +1,64 @@
+import { Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { ButtonProps } from "@/types/type";
+
+const getBgVariantStyle = (variant: ButtonProps["bgVariant"]) => {
+  switch (variant) {
+    case "secondary":
+      return "bg-gray-500";
+    case "danger":
+      return "bg-red-500";
+    case "success":
+      return "bg-green-500";
+    case "outline":
+      return "bg-transparent border-neutral-300 border-[0.5px]";
+    default:
+      return "bg-[#0286ff]";
+  }
+};
+
+const getTextVariantStyle = (variant: ButtonProps["textVariant"]) => {
+  switch (variant) {
+    case "primary":
+      return "text-black";
+    case "secondary":
+      return "text-gray-100";
+    case "danger":
+      return "text-red-100";
+    case "success":
+      return "text-green-100";
+    default:
+      return "text-white";
+  }
+};
+
+export const CustomButton = ({
+  onPress,
+  title,
+  bgVariant = "primary",
+  textVariant = "default",
+  iconLeft: IconLeft,
+  iconRight: IconRight,
+  className,
+  ...props
+}: {
+  onPress?: () => void;
+  title?: string;
+  bgVariant?: ButtonProps["bgVariant"];
+  textVariant?: ButtonProps["textVariant"];
+  iconLeft?: React.JSX.Element | React.ReactNode;
+  iconRight?: React.JSX.Element | React.ReactNode;
+  className?: string;
+}) => (
+  <TouchableOpacity
+    onPress={onPress}
+    className={`w-full rounded-full p-3 flex flex-row justify-center items-center shadow-md shadow-neutral-400/70 ${getBgVariantStyle(bgVariant)} ${className}`}
+    {...props}
+  >
+    {IconLeft}
+    <Text className={`text-lg font-bold ${getTextVariantStyle(textVariant)}`}>
+      {title}
+    </Text>
+    {IconRight}
+  </TouchableOpacity>
+);
